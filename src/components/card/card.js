e05178f4d5f5fed1c61254478bd0cd25bc148468
Akvaro03 <68175898+Akvaro03@users.noreply.github.com>
2022-05-04 22:21:58 -0300

    buscador

---
+++ src/components/card/card.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './card.css';
+import { Card, Button, Modal } from 'react-bootstrap';
+
+function Caard (props) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+        let item = props.item;
+        let foto = props.item.image;
+        return (
+            <div className="contenedor">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={foto} />
+                    <Card.Body>
+                        <Card.Title>{item.name}</Card.Title>
+                        <Card.Text>
+                            Estado: {item.status}
+                        </Card.Text>
+                        <Button variant="primary" onClick={handleShow}>Mas información</Button>
+                    </Card.Body>
+                </Card>   
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+
+            </div>   
+                
+
+        )
+}
+
+export default Caard;
